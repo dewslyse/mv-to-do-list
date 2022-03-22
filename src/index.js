a6756013eeabc0b247b0a7d9820d073ab9b57421
dewslyse <dewslyse@gmail.com>
2022-03-22 22:34:51 +0000

    Render todo list dynamically

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import _ from 'lodash';
 import './style.scss';
 
-function component() {
-    const element = document.createElement('div');
+const todos = [
+  {
+    description: 'Create To Do project',
+    completed: true,
+    index: '1',
+  },
+  {
+    description: 'Set up GitHub Actions',
+    completed: true,
+    index: '2',
+  },
+  {
+    description: 'Set up webpack',
+    completed: true,
+    index: '3',
+  },
+  {
+    description: 'Complete milestone 1 of To Do project',
+    completed: false,
+    index: '4',
+  },
+];
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+// Add Todos to page
+const item = (todo) => `
+    <div class="list-item">
+      <div class="todo-item">
+          <input type="checkbox" id="todo" class="todo" name="todo">
+          <label class="desc" for="todo">${todo.description}</label>
+      </div>
+      <span><i class="fas fa-ellipsis-v"></i></span>
+    </div>
+  `;
 
-    return element;
-}
+const lists = document.querySelector('.lists');
 
-document.body.appendChild(component());
+lists.innerHTML = `
+  ${todos.map(item).join('')}
+`;
