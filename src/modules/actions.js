a7a8f1d6866da96d57648ab0c74d55f3155608a2
dewslyse <dewslyse@gmail.com>
2022-03-25 21:03:20 +0000

    Mark task as completed

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -3,78 +3,91 @@ import Todos from './todos.js';
 const lists = document.querySelector('.lists');
 
 export default class Actions {
-    // Display task on page
-    static displayTask = () => {
-      const todos = Todos.retrieveTask();
+  // Display task on page
+  static displayTask = () => {
+    const todos = Todos.retrieveTask();
 
-      todos.forEach((todo) => {
-        Actions.addNewTask(todo);
-      });
-    }
+    todos.forEach((todo) => {
+      Actions.addNewTask(todo);
+    });
+  }
 
-    // Add and new Task
-    static addNewTask = (todo) => {
-      const listItem = document.createElement('div');
-      listItem.classList.add('list-item');
+  // Add and new Task
+  static addNewTask = (todo) => {
+    const listItem = document.createElement('div');
+    listItem.classList.add('list-item');
 
-      const p = document.createElement('p');
-      p.classList.add('desc');
-      p.id = `${todo.index}`;
+    const p = document.createElement('p');
+    p.classList.add('desc');
+    p.id = `${todo.index}`;
 
-      const check = document.createElement('input');
-      check.type = 'checkbox';
-      check.classList.add('todo');
-      check.setAttribute('aria-label', 'Mark as done');
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    check.classList.add('checkbox');
+    check.setAttribute('aria-label', 'Mark as done');
+    check.checked = todo.completed;
 
-      const taskDescription = document.createElement('span');
-      taskDescription.classList.add('editable');
-      taskDescription.innerText = `${todo.description}`;
-      taskDescription.setAttribute('contenteditable', 'true');
+    const taskDescription = document.createElement('span');
+    taskDescription.classList.add('editable');
+    taskDescription.innerText = `${todo.description}`;
+    taskDescription.setAttribute('contenteditable', 'true');
 
-      const rmBtn = document.createElement('button');
-      rmBtn.classList.add('rm-btn');
-      rmBtn.classList.add('hidden');
-      rmBtn.setAttribute('aria-label', 'Delete task');
-      rmBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    const rmBtn = document.createElement('button');
+    rmBtn.classList.add('rm-btn');
+    rmBtn.classList.add('hidden');
+    rmBtn.setAttribute('aria-label', 'Delete task');
+    rmBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
 
-      const mvbtn = document.createElement('span');
-      mvbtn.classList.add('move');
-      mvbtn.innerHTML = '<i class="fas fa-ellipsis-v">';
+    const mvbtn = document.createElement('span');
+    mvbtn.classList.add('move');
+    mvbtn.innerHTML = '<i class="fas fa-ellipsis-v">';
 
-      p.appendChild(check);
-      p.appendChild(taskDescription);
+    p.appendChild(check);
+    p.appendChild(taskDescription);
 
-      listItem.appendChild(p);
-      listItem.appendChild(rmBtn);
-      listItem.appendChild(mvbtn);
+    listItem.appendChild(p);
+    listItem.appendChild(rmBtn);
+    listItem.appendChild(mvbtn);
 
-      lists.appendChild(listItem);
+    lists.appendChild(listItem);
 
-      // Remove a task
-      rmBtn.addEventListener('click', () => {
-        Todos.removeTask(todo.index);
-        Todos.updateIndex();
-        window.location.reload();
-      });
+    // Remove a task
+    rmBtn.addEventListener('click', () => {
+      Todos.removeTask(todo.index);
+      Todos.updateIndex();
+      window.location.reload();
+    });
 
-      // Update todo text
-      taskDescription.addEventListener('click', () => {
+    // Update todo text
+    taskDescription.addEventListener('click', () => {
+      listItem.classList.toggle('edit');
+      rmBtn.classList.toggle('hidden');
+    });
+
+    taskDescription.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
         listItem.classList.toggle('edit');
         rmBtn.classList.toggle('hidden');
-      });
+        const item = e.target.innerText;
+        Todos.updateTodo(item, todo);
+        taskDescription.blur();
+      }
+    });
 
-      taskDescription.addEventListener('keypress', (e) => {
-        if (e.key === 'Enter') {
-          listItem.classList.toggle('edit');
-          rmBtn.classList.toggle('hidden');
-          const item = e.target.innerText;
-          Todos.updateTodo(item, todo);
-          taskDescription.blur();
-        }
-      });
-    }
+    check.addEventListener('change', (e) => {
+      let state;
+      if (e.target.checked) {
+        state = true;
+        Todos.updateStatus(state, todo);
+      }
+      else {
+        state = false;
+        Todos.updateStatus(state, todo);
+      }
+    });
+  }
 
-    static resetInput = () => {
-      document.querySelector('#add-item').value = '';
-    }
+  static resetInput = () => {
+    document.querySelector('#add-item').value = '';
+  }
 }
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,55 +1,82 @@
 export default class Todos {
-    // Retrieve tasks from localStorage
-    static retrieveTask = () => {
-      let todos;
-      if (localStorage.getItem('todos') === null) {
-        todos = [];
-      } else {
-        todos = JSON.parse(localStorage.getItem('todos'));
+  // Retrieve tasks from localStorage
+  static retrieveTask = () => {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+      todos = [];
+    } else {
+      todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    return todos;
+  }
+
+  // Add a to-do
+  static addTask = (todo) => {
+    const todos = Todos.retrieveTask();
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  // Delete a to-do
+  static removeTask = (id) => {
+    const todos = Todos.retrieveTask();
+    todos.forEach((todo, index) => {
+      if (todo.index === id) {
+        todos.splice(index, 1);
       }
-      return todos;
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  // Update to-do index
+  static updateIndex = () => {
+    const todos = Todos.retrieveTask();
+    let i = 0;
+    for (let j = 0; j < todos.length; j += 1) {
+      i += 1;
+      todos[j].index = i;
     }
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
 
-    // Add a to-do
-    static addTask = (todo) => {
-      const todos = Todos.retrieveTask();
-      todos.push(todo);
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
+  // Update to-do description
+  static updateTodo = (value, todo) => {
+    const todos = Todos.retrieveTask();
+    todos[todo.index - 1].description = value;
 
-    // Delete a to-do
-    static removeTask = (id) => {
-      const todos = Todos.retrieveTask();
-      todos.forEach((todo, index) => {
-        if (todo.index === id) {
-          todos.splice(index, 1);
-        }
-      });
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
 
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
+  // Update completion status
+  static updateStatus = (value, todo) => {
+    const todos = Todos.retrieveTask();
+    todos[todo.index - 1].completed = value;
 
-    // Update to-do index
-    static updateIndex = () => {
-      const todos = Todos.retrieveTask();
-      let i = 0;
-      for (let j = 0; j < todos.length; j += 1) {
-        i += 1;
-        todos[j].index = i;
-      }
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
 
-    // Update to-do description
-    static updateTodo = (value, todo) => {
-      const todos = Todos.retrieveTask();
-      todos[todo.index - 1].description = value;
+  // static updateLocalStorage = () => {
+  //   const todos = Todos.retrieveTask();
+  //   // Todos.updateIndex();
+  //   localStorage.setItem('todos', JSON.stringify(todos));
+  //   // 
+  // }
 
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
+  // Reset todo list
+  static resetAll = () => {
+    localStorage.clear();
+  }
 
-    // Reset todo list
-    static resetAll = () => {
-      localStorage.clear();
-    }
+  //Clear completed
+  // static removeCompleted = () => {
+  //   let todos = Todos.retrieveTask();
+  //   todos.forEach(() => {
+  //     todos = todos.filter((todos) => {
+  //       todos.completed !== true;
+  //     });
+  //   });
+  //   localStorage.setItem('todos', JSON.stringify(todos));
+  //   // Todos.updateIndex();
+  // }
 }
